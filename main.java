b28072ed6b7b3e0ee9336369bdbe4de276488835
DarshanKrishna-DK <darshankrishna2k2@gmail.com>
2024-07-25 19:00:40 +0530

    Git Demo

---
+++ main.java
@@ -0,0 +1,12 @@
+// program to print hello world in java 
+
+class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        int a = 10;
+        a+=20;
+        System.out.println(a);
+    }
+}
