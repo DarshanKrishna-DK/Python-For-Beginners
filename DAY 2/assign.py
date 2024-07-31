361b6bc0ec7e0753a3566ccf92b9c6572ca1f64b
DarshanKrishna-DK <darshankrishna2k2@gmail.com>
2024-07-31 20:17:08 +0530

    Day 3 assignment included

--- DAY 2/assign.py
+++ DAY 2/assign.py
@@ -1,2 +1,19 @@
 #Program to find whether a value is even or odd through quotient - if its not a integer value then print "invalid value"
-#take input from the user into a list 
+
+# Traversing through the list 
+
+list2 = [1,2,5,"string",7,4,3,11,"dk",22]
+
+for val in list2:
+    #checking the type of the value
+    if(type(val) == int):
+        if (type(val/2) == int):
+            print(val, "is even")
+        else:
+            print(val, "is odd")
+    else:   
+        print("invalid value")
+
+
+
+
